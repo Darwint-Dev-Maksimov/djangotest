a01332012ce3f1e0c3a98a1d9cff8cb9f6320e59
Darwint-Dev-Maksimov <maksimov@darwint.ru>
2020-09-17 10:52:12 +0300

    successful connection to the db

--- test_serv/settings.py
+++ test_serv/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'test_serv.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':  'django_db',
+        'USER': 'Dev-Maksimov',
+        'PASSWORD': 'P@ss4Aleksey',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
